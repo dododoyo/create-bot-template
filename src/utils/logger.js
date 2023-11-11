594d92d54e2a41677db4bf20b9ced978758f2673
dododoyo <dolphinmulugeta88@gmail.com>
2023-11-11 22:18:22 +0300

    added colorful

---
+++ src/utils/logger.js
@@ -0,0 +1,26 @@
+class Logger {
+  log(...texts) {
+    console.log(...texts);
+  }
+
+  success(...texts) {
+    this.log("\x1b[32m%s\x1b[0m", "[CREATE-BOT]: ", ...texts); // Green
+  }
+
+  info(...texts) {
+    this.log("\x1b[33m%s\x1b[0m", "[CREATE-BOT]: ", ...texts); // Yellow
+  }
+  question(...texts) {
+    this.log("\x1b[34m%s\x1b[0m", "[CREATE-BOT]", ...texts); // Blue
+  }
+  error(...texts) {
+    this.log("\x1b[31m%s\x1b[0m", "[CREATE-BOT]: ", ...texts); // Red
+  }
+
+  realError(...texts) {
+    this.log("\x1b[41m%s\x1b[0m", "[RealError]: ", ...texts, "\n"); // Red on white background
+  }
+}
+
+const logger = new Logger();
+module.exports = { Logger, logger };
